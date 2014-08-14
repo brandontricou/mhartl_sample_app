0fcc4708c53344f38465d60d8e20b349b8a032db
Brandon Tricou <brandon.tricou@gmail.com>
2014-08-14 15:45:47 -0400

    Add user following

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -5,6 +5,7 @@ describe "Micropost pages" do
   subject { page }
 
   let(:user) { FactoryGirl.create(:user) }
+  let(:other_user) { FactoryGirl.create(:user) }
   before { sign_in user }
 
   describe "micropost creation" do
@@ -40,6 +41,16 @@ describe "Micropost pages" do
       it "should delete a micropost" do
         expect { click_link "delete" }.to change(Micropost, :count).by(-1)
       end
+    end
+
+    describe "as incorrect user" do
+      before do
+        FactoryGirl.create(:micropost, user: other_user)
+        visit users_path
+        click_link "#{other_user.name}"
+      end
+
+      it { should_not have_link('delete') }
     end
   end
 end
